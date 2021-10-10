50c0c17b416265517a14f4826194f6ac12ad2d64
anmol0001 <anmol0556.cse19@chitkara.edu.in>
2021-10-10 17:33:06 +0530

    new_weather_app

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Temp from './NewWeatherApp/temp';                         
+
+ReactDom.render(
+   <Temp/>
+      ,document.getElementById("root"));
